fd8b5b62d9e7155ff7fae9c5422fd77cdcf76134
AncaBazan <115390292+AncaBazan@users.noreply.github.com>
2024-08-12 13:18:11 +0300

    Add files via upload

---
+++ GenericFlyweight/src/ConcreteFlyweight.java
@@ -0,0 +1,12 @@
+public class ConcreteFlyweight implements Flyweight {
+    private String intrinsicState;
+
+    public ConcreteFlyweight(String intrinsicState) {
+        this.intrinsicState = intrinsicState;
+    }
+
+    @Override
+    public void operation() {
+        System.out.println("ConcreteFlyweight: " + intrinsicState);
+    }
+}
